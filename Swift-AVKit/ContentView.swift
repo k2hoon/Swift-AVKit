3dd8aafb2fc415ebf8bcef68afc6f1416c10adb6
k2hoon <tayesymbol@gmail.com>
2023-07-05 22:31:10 +0900

    Initial Commit

---
+++ Swift-AVKit/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Swift-AVKit
+//
+//  Created by k2hoon on 2023/07/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ Swift-AVKit/Swift_AVKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_AVKitApp.swift
+//  Swift-AVKit
+//
+//  Created by k2hoon on 2023/07/05.
+//
+
+import SwiftUI
+
+@main
+struct Swift_AVKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
